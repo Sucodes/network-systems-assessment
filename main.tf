a81744d685cacc0c120024eb9057be14cebdcb94
sucodes <rhounamoney@gmail.com>
2025-11-08 10:12:20 +0000

    add public ip

--- main.tf
+++ main.tf
@@ -26,27 +26,33 @@ resource "google_compute_subnetwork" "subnet" {
 }
 
 resource "google_compute_firewall" "allow_ssh" {
-  name    = "terraform-firewall-allow-ssh"
-  network = google_compute_network.vpc_network.id
+  name        = "terraform-firewall-allow-ssh"
+  network     = google_compute_network.vpc_network.id
+  direction   = "INGRESS"
+  priority    = 1000
+  target_tags = ["ssh"]
 
   allow {
     protocol = "tcp"
     ports    = ["22"]
   }
 
-  source_ranges = [ "0.0.0.0/0"]
+  source_ranges = ["79.140.223.114/32"]
 }
 
 resource "google_compute_firewall" "allow_web" {
-  name    = "terraform-firewall-allow-web"
-  network = google_compute_network.vpc_network.id
+  name        = "terraform-firewall-allow-web"
+  network     = google_compute_network.vpc_network.id
+  direction   = "INGRESS"
+  priority    = 1000
+  target_tags = ["web"]
 
   allow {
     protocol = "tcp"
     ports    = ["80", "443"]
   }
 
-  source_ranges = [ "0.0.0.0/0"]
+  source_ranges = ["0.0.0.0/0"]
 }
 
 resource "google_compute_instance" "vm_instance" {
