3ed13f19a8fb3c7227c554e25c8730b7b5d4aab6
sucodes <rhounamoney@gmail.com>
2025-11-09 03:38:35 +0000

    change port

--- app/server.js
+++ app/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 
-const PORT = 8080;
+const PORT = 80;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
