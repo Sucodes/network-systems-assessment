b5bd0bdd9cea0a7f697d1f6eb64c6da2e7b10cc8
sucodes <rhounamoney@gmail.com>
2025-11-09 05:24:51 +0000

    test app

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World! This is Node.js application running inside a Docker container.");
+  res.send("Hello World! This is Money");
 });
 
 const PORT = 8080;
